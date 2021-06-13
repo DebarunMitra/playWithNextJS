dd232b9e5760a5abe61d3aebc5d54ec11173a564
Debarun Mitra <debarun.mitra@thecareerlabs.com>
2021-06-13 21:30:36 +0530

    base setup updated

---
+++ first-next-app/pages/about.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+const about = ({}) => (
+  <div>
+  <Head>
+    <title>Web Dev About</title>
+    <meta name='keywords' content='web development, programming, about' />
+  </Head>
+    <h1>About Us</h1>
+  </div>
+);
+
+export default about
